53a65ec8e3acd01598af85a93dab123f20ea30fa
Your Name <Your.Name@delphi.com>
2016-01-19 16:28:56 +0800

    add py2exe support

--- setup.py
+++ setup.py
@@ -11,11 +11,20 @@ cx = dict(
     icon_resources = [(0, "icon.ico")],
 )
 
+py2exe_options = {
+        "includes": ["sip"],
+        "dll_excludes": ["MSVCP90.dll",],
+        "compressed": 1,
+        "optimize": 2,
+        "ascii": 0,
+        }
+
 setup(
-    name = "Piczip.",
+    name = "Piczip",
     description = "Compress Picture Size.",
     version = "1.1.0.0",
     # console=[cx],
     # windows=[wx],
-    windows = [{"script":"PiczipWnd_qt.py", "icon_resources": [(0, "icon.ico")]} ]
+    windows = [{"script":"PiczipWnd_qt.py", "icon_resources": [(0, "icon.ico")]} ],
+    options = {'py2exe': py2exe_options}
 )
